a38f14530b306437902432d3064d805535c36516
vaibhavsinghal87 <vaibhavsinghal87@gmail.com>
2022-12-19 20:37:04 +0530

    swagger initial commit

--- app.js
+++ app.js
@@ -3,10 +3,13 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+const expressOasGenerator = require('express-oas-generator');
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+expressOasGenerator.init(app, {});
 
 app.use(logger('dev'));
 app.use(express.json());
